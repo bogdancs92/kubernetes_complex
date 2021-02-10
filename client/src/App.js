35c80f20f55116ac756f7b7d12ac63afb9ea8dda
Bogdan SCANTEIE <bse@MACBSE.local>
2021-02-10 23:22:23 +0100

    skaffold

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other page</Link>
         <div>
-          <h3>Client App Update</h3>
+          <h3>Client App Update live ?</h3>
           <Switch>
             <Route path="/otherpage" component={OtherPage} />
             <Route exact path="/" component={Fib} />
